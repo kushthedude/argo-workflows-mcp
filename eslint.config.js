8a2abe1c0924fd5a7551e686075bab9828d3bb10
sakshi1215 <kushthedude@gmail.com>
2025-07-31 20:14:55 +0530

    feat: init project
    Signed-off-by: sakshi1215 <kushthedude@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+    'prefer-arrow-callback': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    
+    // Error prevention
+    'no-throw-literal': 'error',
+    'no-return-await': 'error',
+    'require-await': 'error',
+    'no-async-promise-executor': 'error',
+    
+    // Code style
+    'curly': ['error', 'multi-line'],
+    'eqeqeq': ['error', 'always'],
+    'no-nested-ternary': 'error',
+    'no-unneeded-ternary': 'error',
+    'spaced-comment': ['error', 'always'],
+  },
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+};
